b3a70c4b7c5fc0e3b8462ebd5677dc0b6160bf4d
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-18 20:57:42 +0900

    Adding hashtable

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace csharpcollection
         {
             //Console.WriteLine("Hello World!");
             ArrayList arrayList = new ArrayList();
+            Hashtable owner = new Hashtable();
+
+            /*
             arrayList.Add(22);
             // can add duplicate
             arrayList.Add(22);
@@ -26,6 +29,22 @@ namespace csharpcollection
             foreach(var item in arrayList)
             {
                 System.Console.WriteLine(item);
+            }
+            */
+
+            // Adding some value in Hashtable
+            owner.Add("Billl", "Microsoft");
+            owner.Add("Paul", "Microsoft");
+            owner.Add("Steve", "Apple");
+            owner.Add("Mark", "Facebook");
+
+            // Accessing one specified by key
+            System.Console.WriteLine("Bill is owner of {0}", owner["Bill"]);
+
+            // Iterate one by one
+            foreach(DictionaryEntry item in owner)
+            {
+                Console.WriteLine("{0}, {1}", item.Key, item.Value);
             }
         }
     }
