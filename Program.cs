553686abe1f006f4768773450887d878b1ba0f2d
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-18 20:51:04 +0900

    Adding ArrayList

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace csharpcollection
 {
@@ -6,7 +7,26 @@ namespace csharpcollection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(22);
+            // can add duplicate
+            arrayList.Add(22);
+            arrayList.Add("Ali");
+            arrayList.Add(true);
+
+            // iterate over each index of arraylist
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                System.Console.WriteLine(arrayList[i]);
+            }
+
+            arrayList.Remove(22);
+            // other way to iterate
+            foreach(var item in arrayList)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
