082838a174629e60d4a31274ec3fd474b9cdd8c3
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-19 13:31:38 +0900

    Buffer and sort

--- Program.cs
+++ Program.cs
@@ -138,7 +138,7 @@ namespace csharpcollection
             Console.ReadLine();
             */
             var numbers = Enumerable.Range(0, 10);
-            var parallelResult = numbers.AsParallel().Where( i => i % 2 == 0).ToArray();
+            var parallelResult = numbers.AsParallel().AsOrdered().Where( i => i % 2 == 0).ToArray();
 
             foreach(int i in parallelResult)
             {
